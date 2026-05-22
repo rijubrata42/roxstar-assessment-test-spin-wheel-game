dbd4d6247f1572a8982ec3ff5fd6a8b1feb20177
Rijubrata Mondal <rijubratamondal@gmail.com>
2026-05-23 01:53:26 +0530

    Changed from individual variables to connection string

--- config/database.js
+++ config/database.js
@@ -2,11 +2,21 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        // Neon and most managed Postgres providers require SSL
+        rejectUnauthorized: false,
+    },
+});
+
+// Test connection on startup
+pool.connect((err, client, release) => {
+    if (err) {
+        console.error("Failed to connect to the database:", err.message);
+    } else {
+        console.log("Database connected successfully");
+        release();
+    }
 });
 
 module.exports = pool;
